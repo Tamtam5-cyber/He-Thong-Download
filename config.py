e15ba38986e22f48e678f717269b765a01427885
kane5 <Nguyenkhactam5@gmail.com>
2025-05-30 23:03:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
-# Licensed under the GNU General Public License v3.0.  
-# See LICENSE file in the repository root for full license text.
-
 import os
 from dotenv import load_dotenv
 
@@ -17,21 +13,20 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
-DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
+API_ID = os.getenv("API_ID", "24523730")
+API_HASH = os.getenv("API_HASH", "c7425e424447932cc25ec8f43a7fbc2d")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7537588681:AAHy45ORqyArvg0nhVo8H8KqQlae2oSNG40")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://nguyenkhactam5:q1231234@superbot.vmvzlsu.mongodb.net/?retryWrites=true&w=majority&appName=SuperBot")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "6798798180").split())) # list seperated via space
+DB_NAME = os.getenv("DB_NAME", "nguyenkhactam5")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002283092497")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002472444792")) # optional with -100
+MASTER_KEY = os.getenv("MASTER_KEY", "d070f1fbcadc95a18b2e8e68512f76fe") # for session encryption
+IV_KEY = os.getenv("IV_KEY", "83eceaf60418") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
-
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "10"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "100"))
+JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/ULTIMATESMMNEWS") # this link for start command message
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/Name_Apex")
